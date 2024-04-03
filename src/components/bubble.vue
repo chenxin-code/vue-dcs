1532f3d15676e2fd01999fbd22c62e9c45113919
chenxin <chenxin@gennlife.com>
2024-04-03 17:41:15 +0800

    气泡消息

--- src/components/bubble.vue
+++ src/components/bubble.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bubble-box" :class="{op: op}" v-show="props.bubbleStr">
-    {{props.bubbleStr}}
+    {{ props.bubbleStr }}
   </div>
 </template>
 
 <script lang="ts" setup>
-import {ref,defineProps} from "vue";
+import {ref, defineProps} from "vue";
 
 const op = ref(false);
 setTimeout(() => {
@@ -22,21 +22,32 @@ const props = defineProps<Inprops>();
 <style lang="less" scoped>
 .bubble-box {
   min-width: 20%;
-  max-width: 40%;
+  max-width: 35%;
   border-radius: 2vw;
   background-color: #fff;
   font-size: 0.7rem;
   text-align: center;
   padding: 0.5rem 0.9rem;
   position: fixed;
-  top: 25%;
+  top: 15%;
   left: 45%;
-  z-index: 3000;
+  z-index: 3001;
   transition: opacity 1.5s ease;
   opacity: 0;
 
   &.op {
     opacity: 1;
   }
+
+  &::after {
+    content: '';
+    border: 0.5rem solid #ffffff00;
+    border-right: 0.5rem solid #fff;
+    transform: translateY(-50%);
+    position: absolute;
+    top: 50%;
+    left: -1rem;
+  }
+
 }
 </style>
