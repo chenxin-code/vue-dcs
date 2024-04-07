d4f21b873f26bce3edfd756fbee4f1ecde82201d
chenxin <chenxin@gennlife.com>
2024-04-07 17:53:22 +0800

    信用卡弹出层

--- src/components/popup/creditCard.vue
+++ src/components/popup/creditCard.vue
@@ -2,22 +2,31 @@
   <div class="popupWindow" @click="closePopup()">
     <div class="popupDialog" @click.stop>
       <div class="box1">
-        <img src="@/assets/img/intoKefu.png"/>
-        <span class="title">建设银行信用卡（3298）</span>
+        <van-icon name="arrow-left" color="rgba(121, 121, 121, 1)" size="1.1rem" @click="showConfirm = false"
+                  v-show="showConfirm"/>
+        <img src="@/assets/img/intoKefu.png" alt=""/>
+        <span class="title" @click="showAllCard()">建设银行信用卡（3298）</span>
+        <van-icon name="cross" color="rgba(121, 121, 121, 1)" size="1.1rem" @click="closePopup()"/>
+        <div v-show="showAllCardBox"></div>
       </div>
-      <div class="box2">
+      <div class="box2" v-show="!showConfirm">
         <div class="tip">信用卡分期利息有7折优惠</div>
         <div class="sum">本期账单合计</div>
         <div class="rmb">¥1000.00</div>
       </div>
-      <div class="box3">
+      <div class="box3" :class="{confirm: showConfirm}">
         <div class="line-item">
           <div>分期金额</div>
           <div>
-            <van-field @focus="inputFocus()" placeholder="请输入分期金额" right-icon="edit" input-align="right"/>
+            <van-field
+                :readonly="showConfirm"
+                @focus="inputFocus()"
+                :placeholder="showConfirm?'':'请输入分期金额'"
+                :right-icon="showConfirm?'':'edit'"
+                input-align="right"/>
           </div>
         </div>
-        <div class="line-item arrow" @click="showPeriodBox = !showPeriodBox">
+        <div class="line-item" :class="{arrow: !showConfirm}" @click="selectPeriod()">
           <div>分期选择</div>
           <div>12期</div>
         </div>
@@ -68,7 +77,7 @@
         </div>
       </div>
       <div class="btn-box">
-        <div class="line1">
+        <div class="line1" v-show="!showConfirm">
           <van-checkbox
               v-model="checked"
               shape="square"
@@ -77,8 +86,9 @@
             <span :style="{color: checked?'rgba(49, 157, 255, 1)':'inherit'}">我已阅读并同意</span>
           </van-checkbox>
         </div>
-        <div class="line2" @click="showTermBox = true">《中国建设银行龙卡信用卡账单分期付款业务约定条款》</div>
-        <button @click="handle()">办理信用卡分期</button>
+        <div class="line2" @click="showTermBox = true" v-show="!showConfirm">《中国建设银行龙卡信用卡账单分期付款业务约定条款》</div>
+        <button @click="handle()" v-show="!showConfirm">办理信用卡分期</button>
+        <button @click="confirmHandle()" v-show="showConfirm">确认办理信用卡分期</button>
       </div>
       <div class="term-box" v-show="showTermBox">
         <div class="title">《中国建设银行龙卡信用卡账单分期 付款业务约定条款》</div>
@@ -105,11 +115,27 @@ const inputFocus = () => {
 const showPeriodBox = ref(false);
 const showTermBox = ref(false);
 const checked = ref(false);
+const showConfirm = ref(false);
+const showAllCardBox = ref(false);
 const handle = () => {
   if (checked.value) {
+    showConfirm.value = true;
     emit("isChecked");
   } else {
     emit("noChecked");
+  }
+};
+const confirmHandle = () => {
+
+};
+const selectPeriod = () => {
+  if (!showConfirm.value) {
+    showPeriodBox.value = !showPeriodBox.value;
+  }
+};
+const showAllCard = () => {
+  if (!showConfirm.value) {
+    showAllCardBox.value = !showAllCardBox.value;
   }
 };
 onMounted(() => {
@@ -154,6 +180,17 @@ onMounted(() => {
       .title {
         font-size: 0.85rem;
         font-weight: bold;
+        position: relative;
+
+        &::after {
+          content: '';
+          border: 0.35rem solid #fff;
+          border-top: 0.35rem solid #000;
+          transform: translateY(-25%);
+          position: absolute;
+          top: 50%;
+          right: -0.7rem;
+        }
       }
 
       img {
@@ -161,6 +198,17 @@ onMounted(() => {
         height: 1.5rem;
         vertical-align: middle;
         margin-right: 0.2rem;
+      }
+
+      .van-icon-cross {
+        cursor: pointer;
+        position: absolute;
+        right: 1.4rem;
+        top: 1.35rem;
+      }
+
+      .van-icon-arrow-left {
+        vertical-align: middle;
       }
     }
 
@@ -210,6 +258,11 @@ onMounted(() => {
     .box3 {
       overflow-y: auto;
       height: 35%;
+      padding: 0.2rem 1.4rem;
+
+      &.confirm {
+        height: 70%;
+      }
 
       .period-box {
         padding: 0 0.2rem;
@@ -247,8 +300,6 @@ onMounted(() => {
           }
         }
       }
-
-      padding: 0.2rem 1.4rem;
 
       .line-item {
         margin: 1.4rem 0;
@@ -393,11 +444,23 @@ onMounted(() => {
         font-size: 1.2rem;
         text-align: center;
         color: rgba(35, 108, 255, 1);
+        position: relative;
+
+        &::after {
+          position: absolute;
+          height: 1px;
+          background-color: rgba(149, 149, 149, 1);
+          content: '';
+          right: 0;
+          bottom: -1rem;
+          left: 0;
+          transform: scaleY(0.13);
+        }
       }
 
       .content {
         margin-bottom: 1rem;
-        padding: 0 1rem;
+        padding: 0 1rem 0.35rem;
         font-size: 0.77rem;
         color: rgba(118, 120, 128, 1);
         overflow-y: auto;
@@ -412,6 +475,17 @@ onMounted(() => {
         bottom: 1rem;
         left: 1rem;
         right: 1rem;
+
+        &::before {
+          position: absolute;
+          height: 1px;
+          background-color: rgba(149, 149, 149, 1);
+          content: '';
+          right: 0;
+          top: -1rem;
+          left: 0;
+          transform: scaleY(0.13);
+        }
 
         > button {
           flex: 1;
